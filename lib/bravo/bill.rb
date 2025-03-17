ee86a8e54dc2627344b714e587726bb85c228bd3
Nicolás Cerrini <ceneon@gmail.com>
2025-03-17 00:14:28 -0300

    unreleased code

--- lib/bravo/bill.rb
+++ lib/bravo/bill.rb
@@ -1,164 +1,183 @@
 module Bravo
   class Bill
     attr_reader :client, :base_imp, :total
-    attr_accessor :net, :doc_num, :iva_cond, :documento, :concepto, :moneda,
-      :due_date, :aliciva_id, :fch_serv_desde, :fch_serv_hasta,
-      :body, :response
+    attr_accessor :net, :doc_num, :iva_cond, :recipient_iva_cond,
+                  :documento, :concepto, :moneda,
+                  :due_date, :fch_serv_desde, :fch_serv_hasta, :fch_emision,
+                  :body, :response, :ivas, :related_invoice_data
 
     def initialize(attrs = {})
       Bravo::AuthData.fetch
+      @client         = Savon::Client.new do
+        wsdl.document = Bravo.service_url
+        http.auth.ssl.cert_key_file = Bravo.pkey
+        http.auth.ssl.cert_file = Bravo.cert
+        http.auth.ssl.verify_mode = :none
+        http.read_timeout = 90
+        http.open_timeout = 90
+        http.headers = { "Accept-Encoding" => "gzip, deflate", "Connection" => "Keep-Alive" }
 
-      @client = Savon.client(
-        env_namespace: :soap,
-        wsdl: Bravo.service_url,
-        ssl_cert_key_file: Bravo.pkey,
-        ssl_cert_file: Bravo.cert,
-        ssl_verify_mode: :none,
-        read_timeout: 90,
-        open_timeout: 90,
-        headers: {
-          'Accept-Encoding' => 'gzip, deflate', 'Connection' => 'Keep-Alive'
-        },
-        log: Bravo.log?,
-        log_level: :debug,
-        namespaces: {xmlns: 'http://ar.gov.afip.dif.FEV1/'}
-      )
+        # Insist because this config doesn't stick from bravo.rb for some reason
+        config.logger = Rails.logger
+        config.log = true
+        config.log_level = :debug
+      end
 
-      @body           = {'Auth' => Bravo.auth_hash}
+      @body           = {"Auth" => Bravo.auth_hash}
       @net            = attrs[:net] || 0
       self.documento  = attrs[:documento] || Bravo.default_documento
       self.moneda     = attrs[:moneda]    || Bravo.default_moneda
       self.iva_cond   = attrs[:iva_cond]
       self.concepto   = attrs[:concepto]  || Bravo.default_concepto
+      self.ivas = attrs[:ivas] || Array.new # [ 1, 100.00, 10.50 ], [ 2, 100.00, 21.00 ] 
     end
 
     def cbte_type
-      Bravo::BILL_TYPE[iva_cond.to_sym] ||
+      Bravo::BILL_TYPE[Bravo.own_iva_cond][iva_cond] ||
         raise(NullOrInvalidAttribute.new, "Please choose a valid document type.")
     end
 
     def exchange_rate
       return 1 if moneda == :peso
-      response = client.call :fe_param_get_cotizacion do
-        message body.merge!({"MonId" => Bravo::MONEDAS[moneda][:codigo]})
+      response = client.request :fe_param_get_cotizacion do
+        soap.namespaces["xmlns"] = "http://ar.gov.afip.dif.FEV1/"
+        soap.body = body.merge!({"MonId" => Bravo::MONEDAS[moneda][:codigo]})
       end
       response.to_hash[:fe_param_get_cotizacion_response][:fe_param_get_cotizacion_result][:result_get][:mon_cotiz].to_f
     end
 
     def total
-      @total = net.zero? ? 0 : (net + iva_sum).round(2)
+      @total = net.zero? ? 0 : net + iva_sum
     end
 
     def iva_sum
-      @iva_sum = net * Bravo::ALIC_IVA[aliciva_id][1]
+      @iva_sum = 0.0
+      self.ivas.each{ |i|
+        @iva_sum += i[1] * Bravo::ALIC_IVA[ i[0] ][1]
+        # @iva_sum += i[2] 
+      }
+      #@iva_sum = net * Bravo::ALIC_IVA[TODO][1]
+      #@iva_sum.round_up_with_precision(2)
       @iva_sum.round(2)
     end
 
     def authorize
-      return false unless setup_bill
-
-      response = client.call(:fecae_solicitar) do |soap|
-        soap.message(body)
+      setup_bill
+      response = client.request :fecae_solicitar do |soap|
+        soap.namespaces["xmlns"] = "http://ar.gov.afip.dif.FEV1/"
+        soap.body = body
       end
 
-      return false unless setup_response(response.to_hash)
+      setup_response(response.to_hash)
       self.authorized?
     end
 
     def setup_bill
-      today = Time.new.in_time_zone('Buenos Aires').strftime('%Y%m%d')
+      if fch_emision then
+        fecha_emision = fch_emision.strftime('%Y%m%d')
+      else
+        fecha_emision = Time.new.strftime('%Y%m%d') #today
+      end
+       
 
-      fecaereq = {
-        'FeCAEReq' => {
-          'FeCabReq' => Bravo::Bill.header(cbte_type),
-          'FeDetReq' => {
-            'FECAEDetRequest' => {
-              'Concepto'    => Bravo::CONCEPTOS[concepto],
-              'DocTipo'     => Bravo::DOCUMENTOS[documento],
-              'CbteFch'     => today,
-              'ImpTotConc'  => 0.00,
-              'MonId'       => Bravo::MONEDAS[moneda][:codigo],
-              'MonCotiz'    => exchange_rate,
-              'ImpOpEx'     => 0.00,
-              'ImpTrib'     => 0.00,
-              'Iva'         => {
-                'AlicIva' => {
-                  'Id' => '5',
-                  'BaseImp' => net,
-                  'Importe' => iva_sum
-                }
-              }
-            }
-          }
-        }
+      array_ivas = Array.new
+      self.ivas.each{ |i|
+          array_ivas << {
+              "Id" => Bravo::ALIC_IVA[ i[0] ][0],
+              "BaseImp" => i[1] ,
+              "Importe" => i[2] }
       }
 
-      detail = fecaereq['FeCAEReq']['FeDetReq']['FECAEDetRequest']
+      fecaereq = {"FeCAEReq" => {
+                    "FeCabReq" => Bravo::Bill.header(cbte_type),
+                    "FeDetReq" => {
+                      "FECAEDetRequest" => {
+                        "Concepto"    => Bravo::CONCEPTOS[concepto],
+                        "DocTipo"     => Bravo::DOCUMENTOS[documento],
+                        "CbteFch"     => fecha_emision,
+                        "ImpTotConc"  => 0.00,
+                        "MonId"       => Bravo::MONEDAS[moneda][:codigo],
+                        "MonCotiz"    => exchange_rate,
+                        "ImpOpEx"     => 0.00,
+                        "ImpTrib"     => 0.00
+                    }}}}
 
-      detail['DocNro']    = self.doc_num
-      detail['ImpNeto']   = self.net.to_f
-      detail['ImpIVA']    = self.iva_sum
-      detail['ImpTotal']  = self.total
-
-      if bill_number = next_bill_number
-        detail['CbteDesde'] = detail['CbteHasta'] = bill_number
-      else
-        return false
+      if Bravo.own_iva_cond == :responsable_inscripto
+        fecaereq["FeCAEReq"]["FeDetReq"]["FECAEDetRequest"]["Iva"] = { "AlicIva" => array_ivas }
+      elsif Bravo.own_iva_cond == :responsable_monotributo and array_ivas.any?
+        raise "No incluir montos iva al facturar como Responsable Monotributo"
       end
 
-      unless concepto == 0
-        detail.merge!({'FchServDesde' => fch_serv_desde || today,
-                       'FchServHasta'  => fch_serv_hasta || today,
-                       'FchVtoPago'    => due_date       || today})
+      self.recipient_iva_cond = parse_iva_cond(self.recipient_iva_cond) if self.recipient_iva_cond.is_a?(Symbol)
+      if not self.recipient_iva_cond.is_a?(Integer)
+        raise "La condición IVA del receptor es un campo requerido desde 04/2025"
       end
 
+      detail = fecaereq["FeCAEReq"]["FeDetReq"]["FECAEDetRequest"]
+
+      detail["DocNro"]    = doc_num
+      detail["CondicionIVAReceptorId"] = self.recipient_iva_cond
+      detail["ImpNeto"]   = net.to_f
+      detail["ImpIVA"]    = iva_sum
+      detail["ImpTotal"]  = total.round(2)
+      detail["CbteDesde"] = detail["CbteHasta"] = next_bill_number
+
+      if related_invoice_data
+        detail["CbtesAsoc"] = {
+          "CbteAsoc" => [{
+            "Tipo" => related_invoice_data[:type],
+            "PtoVta" => related_invoice_data[:sale_point],
+            "Nro" => related_invoice_data[:number],
+            "Cuit" => related_invoice_data[:cuit],
+            "CbteFch" => related_invoice_data[:date]
+          }]
+        }
+      elsif ["02", "03", "07", "08"].include?(cbte_type) # NC/ND only
+        detail["PeriodoAsoc"] = {
+          "FchDesde" => fch_emision.beginning_of_month.strftime('%Y%m%d'),
+          "FchHasta" => fch_emision.strftime('%Y%m%d')
+        }
+      end
+
+      unless concepto == "Productos" # En "Productos" ("01"), si se mandan estos parámetros la afip rechaza.
+        detail.merge!({"FchServDesde" => fch_serv_desde || today,
+                      "FchServHasta"  => fch_serv_hasta || today,
+                      "FchVtoPago"    => due_date       || today})
+      end
 
       body.merge!(fecaereq)
-      true
     end
 
     def next_bill_number
-      resp = client.call :fe_comp_ultimo_autorizado do |soap|
-        soap.message(body.merge({"PtoVta" => Bravo.sale_point, "CbteTipo" => cbte_type}))
+      resp = client.request :fe_comp_ultimo_autorizado do
+        soap.namespaces["xmlns"] = "http://ar.gov.afip.dif.FEV1/"
+        soap.body = {"Auth" => Bravo.auth_hash, "PtoVta" => Bravo.sale_point, "CbteTipo" => cbte_type}
       end
 
       resp.to_hash[:fe_comp_ultimo_autorizado_response][:fe_comp_ultimo_autorizado_result][:cbte_nro].to_i + 1
     end
 
-    def authorized?
-      !response.nil? && response.header_result == "A" && response.detail_result == "A"
+    def authorized?       
+        !response.nil? && response.header_result == "A" && response.detail_result == "A"
     end
 
-    def query_bill(iva_condition, cbte_number)
-      cbte_type_selected = Bravo::BILL_TYPE[iva_condition] ||
-        raise(NullOrInvalidAttribute.new, 'Please choose a valid document type.')
-
-      response = client.call :fe_comp_consultar do |soap|
-        soap.message(body.merge({
-          'FeCompConsReq' => {
-            'PtoVta'    => Bravo.sale_point,
-            'CbteTipo'  => cbte_type_selected,
-            'CbteNro'   => cbte_number
-          }
-        }))
-      end
-
-      result = response.to_hash[:fe_comp_consultar_response][:fe_comp_consultar_result][:result_get]
-
-      response_hash = {header_result: result[:resultado],
-                       authorized_on: result[:fch_proceso],
-                       detail_result: result[:resultado],
-                       cae_due_date: result[:fch_vto],
-                       cae: result[:cod_autorizacion],
-                       iva_id: result[:iva][:alic_iva][:id],
-                       iva_importe: result[:iva][:alic_iva][:importe],
-                       moneda: result[:mon_id],
-                       cotizacion: result[:mon_cotiz],
-                       iva_base_imp: result[:iva][:alic_iva][:base_imp],
-                       doc_num: result[:doc_nro]
+    def search(cbte_type, cbte_nro, pto_vta)
+      fecompconsultarreq = {
+        "FeCompConsReq" => {
+          "CbteTipo" => cbte_type,
+          "CbteNro" => cbte_nro,
+          "PtoVta" => pto_vta
+        }
       }
+      body.merge!(fecompconsultarreq)
 
-      self.response = Response.new(response_hash)
+      response = client.request("FECompConsultar", soap_action: "http://ar.gov.afip.dif.FEV1/FECompConsultar") do |soap|
+        soap.namespaces["xmlns"] = "http://ar.gov.afip.dif.FEV1/"
+        soap.body = body
+      end
+      puts "FECompConsultar --> "
+      puts body
+      return response.to_json
     end
 
     private
@@ -170,42 +189,64 @@ module Bravo
     end
 
     def setup_response(response)
-      begin
-        result          = response[:fecae_solicitar_response][:fecae_solicitar_result]
+      # TODO: turn this into an all-purpose Response class
 
-        response_header = result[:fe_cab_resp]
-        response_detail = result[:fe_det_resp][:fecae_det_response]
+      result          = response[:fecae_solicitar_response][:fecae_solicitar_result]
+          
+      if not result[:fe_det_resp] or not result[:fe_cab_resp] then 
+      # Si no obtuvo respuesta ni cabecera ni detalle, evito hacer '[]' sobre algo indefinido.                        
+      # Ejemplo: Error con el token-sign de WSAA
+          keys, values = {
+                :errores => result[:errors],
+                :header_result => {:resultado => "X" },
+                :observaciones => nil
+          }.to_a.transpose
+          self.response = (defined?(Struct::ResponseMal) ? Struct::ResponseMal : Struct.new("ResponseMal", *keys)).new(*values)         
+          return
+      end       
+      
+      response_header = result[:fe_cab_resp]
+      response_detail = result[:fe_det_resp][:fecae_det_response]
 
-        request_header  = body["FeCAEReq"]["FeCabReq"].underscore_keys.symbolize_keys
-        request_detail  = body["FeCAEReq"]["FeDetReq"]["FECAEDetRequest"].underscore_keys.symbolize_keys
-      rescue NoMethodError
-        if defined?(RAILS_DEFAULT_LOGGER) && logger = RAILS_DEFAULT_LOGGER
-          logger.error "[BRAVO] NoMethodError: Response #{response}"
-        else
-          puts "[BRAVO] NoMethodError: Response #{response}"
-        end
-      return false
+      request_header  = body["FeCAEReq"]["FeCabReq"].underscore_keys.symbolize_keys
+      request_detail  = body["FeCAEReq"]["FeDetReq"]["FECAEDetRequest"].underscore_keys.symbolize_keys
+      
+      # Esto no funciona desde que se soportan múltiples alícuotas de iva simultáneas
+      # FIX ? TO-DO
+      # iva             = request_detail.delete(:iva)["AlicIva"].underscore_keys.symbolize_keys
+      # request_detail.merge!(iva) 
+         
+      if result[:errors] then
+          response_detail.merge!( result[:errors] )     
       end
+      
+      response_hash = {:header_result => response_header.delete(:resultado),
+                       :authorized_on => response_header.delete(:fch_proceso),
+                       :detail_result => response_detail.delete(:resultado),
+                       :cae_due_date  => response_detail.delete(:cae_fch_vto),
+                       :cae           => response_detail.delete(:cae),
+                       :iva_id        => request_detail.delete(:id),
+                       :iva_importe   => request_detail.delete(:importe),
+                       :moneda        => request_detail.delete(:mon_id),
+                       :cotizacion    => request_detail.delete(:mon_cotiz),
+                       :iva_base_imp  => request_detail.delete(:base_imp),
+                       :doc_num       => request_detail.delete(:doc_nro), 
+                       :observaciones => response_detail.delete(:observaciones),
+                       :errores       => response_detail.delete(:err) 
+                       }.merge!(request_header).merge!(request_detail)
 
-      iva = request_detail.delete(:iva)["AlicIva"].underscore_keys.symbolize_keys
+      keys, values  = response_hash.to_a.transpose
+      
+      #self.response = (defined?(Struct::Response) ? Struct::Response : Struct.new("Response", *keys)).new(*values)
+      # Even if it throws a warning, it avoids some parsing errors.
+      self.response = Struct.new("Response", *keys).new(*values)
+    end
 
-      request_detail.merge!(iva)
-
-      response_hash = {
-        header_result: response_header.delete(:resultado),
-        authorized_on: response_header.delete(:fch_proceso),
-        detail_result: response_detail.delete(:resultado),
-        cae_due_date:  response_detail.delete(:cae_fch_vto),
-        cae:           response_detail.delete(:cae),
-        iva_id:        request_detail.delete(:id),
-        iva_importe:   request_detail.delete(:importe),
-        moneda:        request_detail.delete(:mon_id),
-        cotizacion:    request_detail.delete(:mon_cotiz),
-        iva_base_imp:  request_detail.delete(:base_imp),
-        doc_num:       request_detail.delete(:doc_nro)
-      }.merge!(request_header).merge!(request_detail)
-
-      self.response = Response.new(response_hash)
+    def parse_iva_cond(sym)
+      return 1 if sym == :responsable_inscripto
+      return 6 if sym == :monotributo
+      return 5 if sym == :consumidor_final
+      return 4 if sym == :exento
     end
   end
 end
